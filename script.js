bdf98edba2b4dee9517bfc28418d717cc045ff58
lucascmelo <lucas.cavalcanti.melo@gmail.com>
2012-11-23 17:43:31 -0300

    comecando a desenvolver :D

---
+++ script.js
@@ -0,0 +1,21 @@
+;(function _eventTrackingGA(){
+	var type,
+		categories,
+		actions,
+		labels,
+		values;
+
+
+
+})();
+
+function gaPush(type,categories,actions,labels,values,){
+	 _gaq.push([type,categories,actions,labels,values]);
+}
+
+function getElemAttr(attr){
+	tag = document.getElementsByTagName("*");
+	for (var i = 0; i < tag.length; i++) {
+		Things[i]
+	};
+}
